0839d01e202cbb1a5817a03a583ef95e9ea7bf6a
kartikdudhaiya <139414433+kartikdudhaiya@users.noreply.github.com>
2023-07-24 14:04:28 +0530

    Initial commit

---
+++ react-hook-sample-website/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sample button with React Hooks</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
